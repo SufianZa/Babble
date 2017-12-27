99607e21470cc31bca35ba589ca3f9e05c2b79fb
DESKTOP-1GIDLUK\Sufian <SufianZa>
2017-12-27 20:48:38 +0100

    commit 2

---
+++ src/main/java/de/unidue/inf/is/utils/DBUtil.java
@@ -0,0 +1,85 @@
+package de.unidue.inf.is.utils;
+
+import com.ibm.db2.jcc.DB2Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+
+
+
+public final class DBUtil {
+
+    private DBUtil() {
+    }
+
+
+    static {
+        com.ibm.db2.jcc.DB2Driver driver = new DB2Driver();
+        try {
+            Class.forName("com.ibm.db2.jcc.DB2Driver");
+            DriverManager.registerDriver(driver);
+        }
+        catch (SQLException e) {
+            throw new Error("Laden des Datenbanktreiber nicht möglich");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static Connection getConnection(String database) throws SQLException {
+        final String url = "jdbc:db2:" + database;
+        return DriverManager.getConnection(url);
+    }
+
+
+    public static Connection getExternalConnection(String database) throws SQLException {
+        Properties properties = new Properties();
+        properties.setProperty("securityMechanism",
+                        Integer.toString(com.ibm.db2.jcc.DB2BaseDataSource.USER_ONLY_SECURITY));
+        properties.setProperty("user", "dbp66");
+        properties.setProperty("password", "vohr9uaw");
+
+        final String url = "jdbc:db2://helios.is.inf.uni-due.de:50066/" + database + ":currentSchema=dbp66;";
+        System.out.println(url);
+        Connection connection = DriverManager.getConnection(url, properties);
+        return connection;
+    }
+
+
+    public static boolean checkDatabaseExistsExternal(String database) {
+        // Nur für Demozwecke!
+        boolean exists = false;
+
+        try (Connection connection = getExternalConnection(database)) {
+            exists = true;
+        }
+        catch (SQLException e) {
+            exists = false;
+            e.printStackTrace();
+        }
+
+        return exists;
+    }
+
+
+    public static boolean checkDatabaseExists(String database) {
+        // Nur für Demozwecke!
+        boolean exists = false;
+
+        try (Connection connection = getConnection(database)) {
+            exists = true;
+        }
+        catch (SQLException e) {
+            exists = false;
+            e.printStackTrace();
+        }
+
+        return exists;
+    }
+
+}
